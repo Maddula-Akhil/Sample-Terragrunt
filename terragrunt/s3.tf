d8bae3bcaaa61cb9a58fe0d2bc0fbc1c772409ca
root <root@ip-172-31-93-213.ec2.internal>
2023-04-21 06:33:33 +0000

    Adding s3 and cloudfront templates

---
+++ terragrunt/s3.tf
@@ -0,0 +1,89 @@
+# Variables
+variable "private_bucket_1_name" {
+  type    = string
+  default = "storage-bucket-promact"
+}
+
+variable "private_bucket_2_name" {
+  type    = string
+  default = "static-site-bucket-promact"
+}
+
+
+
+
+# The bucket storage_bucket is connected to CloudFront.
+
+resource "aws_s3_bucket" "storage_bucket" {
+  bucket = var.private_bucket_1_name
+}
+
+resource "aws_s3_bucket_public_access_block" "storage_bucket_access" {
+  bucket = aws_s3_bucket.storage_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "static_site_bucket" {
+  bucket = var.private_bucket_2_name
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site_bucket_access" {
+  bucket = aws_s3_bucket.static_site_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = true
+}
+
+
+# This cloudfront is connected to storage_bucket.
+
+resource "aws_cloudfront_distribution" "storage_bucket_cloudfront" {
+  origin {
+    domain_name = aws_s3_bucket.storage_bucket.bucket_regional_domain_name
+    origin_id   = "my-private-bucket"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My CloudFront distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "my-private-bucket"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+}
+
