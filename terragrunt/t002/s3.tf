bc03d1cd8d04bfaecb4aa77c67d031d936cc7630
root <root@ip-172-31-93-213.ec2.internal>
2023-05-02 05:50:28 +0000

    editing the S3 template in t002

--- terragrunt/t002/s3.tf
+++ terragrunt/t002/s3.tf
@@ -1,26 +1,22 @@
-variable "bucket_name" {
+# Variables
+variable "private_bucket_1_name" {
   type    = string
-  default = ""
+  default = "storage-bucket-backend-promact"
 }
 
-variable "region" {
+variable "private_bucket_2_name" {
   type    = string
-  default = "us-east-1"
+  default = "static-site-bucket-backend-promact"
 }
 
-provider "aws" {
-  region = var.region
+# Resources
+
+resource "aws_s3_bucket" "storage_bucket_backend" {
+  bucket = var.private_bucket_1_name
 }
 
-
-
-
-resource "aws_s3_bucket" "storage_bucket" {
-  bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_public_access_block" "storage_bucket_access" {
-  bucket = aws_s3_bucket.storage_bucket.id
+resource "aws_s3_bucket_public_access_block" "storage_bucket_backend_access" {
+  bucket = aws_s3_bucket.storage_bucket_backend.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -29,98 +25,69 @@ resource "aws_s3_bucket_public_access_block" "storage_bucket_access" {
 }
 
 
-#resource "aws_s3_bucket" "Static_bucket" {
-#  bucket = "static-host-bucket-promact"
-#
-#  website {
-#    index_document = "index.html"
-#    error_document = "error.html"
-#  }
-#
-#  tags = {
-#    Name = "Static site hosting Bucket"
-#  }
-#
-#  # Add a bucket policy to allow public access
-#  policy = <<POLICY
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [
-#    {
-#      "Effect": "Allow",
-#      "Principal": "*",
-#      "Action": [
-#        "s3:GetObject"
-#      ],
-#      "Resource": [
-#        "arn:aws:s3:::static-host-bucket-promact/*"
-#      ]
-#    }
-#  ]
-#}
-#POLICY
-#}
-#
-#resource "aws_s3_bucket_object" "index_file" {
-#  bucket = aws_s3_bucket.Static_bucket.id
-#  key    = "index.html"
-#  #source       = "/home/akhil/files/index.html"
-#  content_type = "text/html"
-#}
-#
-#resource "aws_s3_bucket_object" "error_file" {
-#  bucket = aws_s3_bucket.Static_bucket.id
-#  key    = "error.html"
-#  #source       = "/home/akhil/files/error.html"
-#  content_type = "text/html"
-#}
-#
-#
-#
-## Define the CloudFront distribution
-#resource "aws_cloudfront_distribution" "static_website_distribution" {
-#  origin {
-#    domain_name = aws_s3_bucket.Static_bucket.website_endpoint
-#    origin_id   = "static-host-bucket-promact"
-#  }
-#  
-#  enabled             = true
-#  is_ipv6_enabled     = true
-#  comment             = "Static website distribution"
-#  default_root_object = "index.html"
-#  
-#  default_cache_behavior {
-#    allowed_methods  = ["GET", "HEAD"]
-#    cached_methods   = ["GET", "HEAD"]
-#    target_origin_id = "static-host-bucket-promact"
-#    
-#    forwarded_values {
-#      query_string = false
-#      
-#      cookies {
-#        forward = "none"
-#      }
-#    }
-#    
-#    viewer_protocol_policy = "redirect-to-https"
-#    min_ttl                = 0
-#    default_ttl           = 86400
-#    max_ttl                = 31536000
-#  }
-#  
-#  viewer_certificate {
-#    cloudfront_default_certificate = true
-#  }
-#  
-#  restrictions {
-#    geo_restriction {
-#      restriction_type = "none"
-#    }
-#  }
-#  
-#  tags = {
-#    Name = "static-host-bucket-promact-distribution"
-#  }
-#}
-#
-#
+# The bucket static_site_bucket_backend is connected to CloudFront.
+
+resource "aws_s3_bucket" "static_site_bucket_backend" {
+  bucket = var.private_bucket_2_name
+}
+
+resource "aws_s3_bucket_public_access_block" "static_site_bucket_backend_access" {
+  bucket = aws_s3_bucket.static_site_bucket_backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = true
+}
+
+
+# This cloudfront is connected to static_site_bucket_backend.
+
+
+
+
+
+resource "aws_cloudfront_distribution" "static_site_bucket_backend_cloudfront" {
+  origin {
+    domain_name = aws_s3_bucket.static_site_bucket_backend.bucket_regional_domain_name
+    origin_id   = "backend-cdn"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My CloudFront distribution"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "backend-cdn"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_100"
+}
+
