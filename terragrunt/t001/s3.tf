f9746d59bf3c3d3aa10e6e30da60c711de76d71b
Maddula-Akhil <akhilamp0000@gmail.com>
2023-05-23 16:40:27 +0530

    Update s3.tf

--- terragrunt/t001/s3.tf
+++ terragrunt/t001/s3.tf
@@ -1,13 +1,13 @@
 # Variables
 variable "private_bucket_1_name" {
   type    = string
-  default = "storage-bucket-promact"
+  default = "storage-bucket-promact-akhil"
   description = "The name of the first private S3 bucket to create."
 }
 
 variable "private_bucket_2_name" {
   type    = string
-  default = "static-site-bucket-promact"
+  default = "static-site-bucket-promact-akhil"
   description = "The name of the second private S3 bucket to create."
 }
 
