2290d31127e723bb924d1d2101dd441c5e52d4c2
Maddula-Akhil <akhilamp0000@gmail.com>
2023-05-30 19:09:08 +0530

    Update rds.tf

--- terragrunt/t001/rds.tf
+++ terragrunt/t001/rds.tf
@@ -37,13 +37,13 @@ variable "db_name" {
 #variable "db_username" {
 #  type        = string
 #  description = "The username to use when connecting to the RDS instance."
-# default     = "yanki-db"
+# default     = "akhil_db"
 #}
 
 #variable "db_password" {
 #  type        = string
 #  description = "The password to use when connecting to the RDS instance."
-#  default     = "YankiDB23052023"
+#  default     = "akhil_dbW123456W"
 #}
 
 variable "db_multi_az" {
